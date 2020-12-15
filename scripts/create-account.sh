baad560c225a390eed1c5b43a9e87cf8de6a17f0
efreeti <slip777sg@gmail.com>
2020-12-15 21:40:57 +0100

    Fixed scripts.

--- scripts/create-account.sh
+++ scripts/create-account.sh
@@ -1,3 +1,3 @@
-DATA = "{\"initialBalance\": \"${1:-'100.00'}\"}"
+DATA="{\"initialBalance\": \"${1:-100.00}\"}"
 
-curl -X POST -d $DATA -H 'Content-Type: application/json' http://localhost:10000/accounts
+curl -X POST -d "$DATA" -H 'Content-Type: application/json' http://localhost:10000/accounts
--- scripts/create-order.sh
+++ scripts/create-order.sh
@@ -1,3 +1,3 @@
-DATA = "{\"accountId\": \"$1\", \"orderType\": \"${2:-'BUY'}\", \"instrumentId\": \"${3:-'GOOG'}\", \"quantity\": ${4:-'1'}, \"price\": \"${5:-'40.00'}\"}"
+DATA="{\"accountId\": \"$1\", \"orderType\": \"${2:-BUY}\", \"instrumentId\": \"${3:-GOOG}\", \"quantity\": ${4:-1}, \"price\": \"${5:-40.00}\"}"
 
-curl -X POST -d $DATA -H 'Content-Type: application/json' http://localhost:10001/orders
+curl -X POST -d "$DATA" -H 'Content-Type: application/json' http://localhost:10001/orders
