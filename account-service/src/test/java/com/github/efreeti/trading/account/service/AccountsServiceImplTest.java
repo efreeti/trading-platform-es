4f8afc41ea0b728d10c54401544a407cf697951b
efreeti <slip777sg@gmail.com>
2020-12-15 17:25:02 +0100

    Fixed the running of tests in CLI.

--- account-service/src/test/java/com/github/efreeti/trading/account/service/AccountsServiceImplTest.java
+++ account-service/src/test/java/com/github/efreeti/trading/account/service/AccountsServiceImplTest.java
@@ -19,7 +19,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -35,9 +34,10 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
 @ExtendWith(SpringExtension.class)
 public class AccountsServiceImplTest {
 	@Mock
